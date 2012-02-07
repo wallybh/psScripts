35495efdad73703bf473c02381ca3dfa70e82927
Wallison Santos <wrsantos@live.com>
2012-02-07 15:23:59 -0200

    Adicionando modulos iniciais para o ambiente do PS.

---
+++ setup/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+Set-Location "C:\Users\wallison.santos\psScripts"
+
+Import-Module PowerTab
+
+Import-Module Pscx
+
+function Import-VS10Vars
+{
+    $vcargs = ?: {$Pscx:Is64BitProcess} {'amd64'} {'x86'}
+    Push-EnvironmentBlock -Description "Before importing VS 2010 $vcargs vars"
+    Invoke-BatchFile "${env:VS100COMNTOOLS}..\..\VC\vcvarsall.bat" $vcargs
+}
+
+Import-VS10Vars
---
+++ setup/README.txt
@@ -0,0 +1,10 @@
+Scripts que eu estou usando:
+
+http://pscx.codeplex.com/ 2.0.0.1 
+
+
+http://powertab.codeplex.com 0.99.6
+
+
+Arquivo Microsoft.PowerShell_profile.ps1
+
