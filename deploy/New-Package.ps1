da662bd7d70f577bee9a3c9437a443529b084b57
Wallison Santos <wrsantos@live.com>
2012-02-07 23:48:23 -0200

    Atualizando New-Package

--- deploy/New-Package.ps1
+++ deploy/New-Package.ps1
@@ -1,12 +1,24 @@
-
-params
+#require MODULE Pscx
+param
 (
 	[String]
-	$Path = $(throw "Parametro -Path não pode ser nulo"),
+	$Path = $(throw "-Path é obrigatório."),
 	[String]
-	$Output = $(throw "Parametro -Output não pode ser nulo"),
+	$Output = $(throw "-Output é obrigatório."),
 	[Int32]
 	$Level = 1
 )
+
+if(-not $Output.EndsWith(".zip"))
+{
+	$Output += ".zip"
+}
+
+
+if($Path -eq $Output)
+{
+	write "Parâmetros -Path e -Output não podem ser iguais."
+	return
+}
 
 write-zip $Path -OutPutPath $Output -Level $Level
